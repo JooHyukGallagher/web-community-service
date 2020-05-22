281860724e13659843fd33d6023ebe8ea9dcdaf9
joohyuk <vfrvfr4207@gmail.com>
2020-05-22 21:03:54 +0900

    remember me

--- src/main/java/me/weekbelt/community/infra/config/SecurityConfig.java
+++ src/main/java/me/weekbelt/community/infra/config/SecurityConfig.java
@@ -1,35 +1,55 @@
 package me.weekbelt.community.infra.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 
+import javax.sql.DataSource;
+
+@RequiredArgsConstructor
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final UserDetailsService userDetailsService;
+    private final DataSource dataSource;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         // 인가 API
-        http
-                .authorizeRequests()
+        http.authorizeRequests()
                 .mvcMatchers("/", "/login", "/sign-up",
                         "/check-email-token", "login-by-email", "/email-login", "/check-email-login",
                         "/login-link", "/search/board/**").permitAll()
                 .mvcMatchers(HttpMethod.GET, "/profile/*").permitAll()
                 .anyRequest().authenticated();
 
-        http
-                .formLogin()
-                .loginPage("/login")
+        http.formLogin()
+                .loginPage("/login")        // 로그인페이지 login.html 커스텀
                 .permitAll();
 
-        http
-                .logout()
+        http.logout()
                 .logoutSuccessUrl("/");
+
+        http.rememberMe()
+                .userDetailsService(userDetailsService)
+                .tokenRepository(tokenRepository());        // token 저장소를 JDBC기반 토큰 저장소로 설정
+    }
+
+    @Bean
+    public PersistentTokenRepository tokenRepository() {
+        JdbcTokenRepositoryImpl jdbcTokenRepository = new JdbcTokenRepositoryImpl();
+        jdbcTokenRepository.setDataSource(dataSource);
+        return jdbcTokenRepository;
     }
 
     @Override
