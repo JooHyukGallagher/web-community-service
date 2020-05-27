b65346640c6c5f24c723ce1245d0fa03a32e9e45
joohyuk <vfrvfr4207@gmail.com>
2020-05-28 05:22:34 +0900

    board read

--- src/main/java/me/weekbelt/community/modules/board/controller/BoardController.java
+++ src/main/java/me/weekbelt/community/modules/board/controller/BoardController.java
@@ -3,16 +3,15 @@ package me.weekbelt.community.modules.board.controller;
 import lombok.RequiredArgsConstructor;
 import me.weekbelt.community.modules.account.Account;
 import me.weekbelt.community.modules.account.CurrentAccount;
-import me.weekbelt.community.modules.board.form.BoardListElementForm;
 import me.weekbelt.community.modules.board.form.BoardWriteForm;
 import me.weekbelt.community.modules.board.service.BoardService;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RequiredArgsConstructor
 @Controller
@@ -20,14 +19,25 @@ public class BoardController {
 
     private final BoardService boardService;
 
-    @GetMapping("/boards/{boardType}")
+    @GetMapping("/boards")
     public String boards(@CurrentAccount Account account, @PageableDefault Pageable pageable,
-                         @PathVariable String boardType, Model model) {
+                         @RequestParam String boardType, Model model) {
         model.addAttribute("account", account);
         model.addAttribute("boards", boardService.findBoardList(boardType, pageable));
         model.addAttribute("boardType", boardType);
         return "board/" + boardType;
     }
+
+    @GetMapping("/boards/{id}")
+    public String readBoard(@CurrentAccount Account account, @PathVariable Long id,
+                            Model model) {
+        model.addAttribute("account", account);
+        model.addAttribute("boardReadForm", boardService.findBoardReadFormById(id));
+        return "board/readForm";
+    }
+//
+//    @GetMapping("/boards/{id}/update")
+//    public
 
     @GetMapping("/new-board")
     public String createNewBoard(@CurrentAccount Account account, Model model) {
