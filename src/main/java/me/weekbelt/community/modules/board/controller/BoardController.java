f5ff008d61d0df0b17dabd4902fda1bf85c58eee
joohyuk <vfrvfr4207@gmail.com>
2020-05-28 22:49:55 +0900

    board update view

--- src/main/java/me/weekbelt/community/modules/board/controller/BoardController.java
+++ src/main/java/me/weekbelt/community/modules/board/controller/BoardController.java
@@ -3,15 +3,20 @@ package me.weekbelt.community.modules.board.controller;
 import lombok.RequiredArgsConstructor;
 import me.weekbelt.community.modules.account.Account;
 import me.weekbelt.community.modules.account.CurrentAccount;
+import me.weekbelt.community.modules.board.form.BoardReadForm;
 import me.weekbelt.community.modules.board.form.BoardWriteForm;
 import me.weekbelt.community.modules.board.service.BoardService;
+import org.dom4j.rule.Mode;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.validation.Valid;
 
 @RequiredArgsConstructor
 @Controller
@@ -35,9 +40,28 @@ public class BoardController {
         model.addAttribute("boardReadForm", boardService.findBoardReadFormById(id));
         return "board/readForm";
     }
-//
-//    @GetMapping("/boards/{id}/update")
-//    public
+
+    @GetMapping("/boards/{id}/update")
+    public String updateBoardForm(@CurrentAccount Account account, @PathVariable Long id,
+                                  Model model) {
+        BoardReadForm boardReadForm = boardService.findBoardReadFormById(id);
+        BoardWriteForm boardWriteForm = BoardWriteForm.builder()
+                .title(boardReadForm.getTitle())
+                .boardType(boardReadForm.getBoardType())
+                .content(boardReadForm.getContent())
+                .build();
+        model.addAttribute("account", account);
+        model.addAttribute("boardWriteForm", boardWriteForm);
+        model.addAttribute("id", id);
+        return "board/updateForm";
+    }
+
+    @PostMapping("/boards/{id}/update")
+    public String updateBoardSubmit(@CurrentAccount Account account, @PathVariable Long id,
+                                    @Valid BoardWriteForm boardWriteForm,
+                                    Model model){
+
+    }
 
     @GetMapping("/new-board")
     public String createNewBoard(@CurrentAccount Account account, Model model) {
