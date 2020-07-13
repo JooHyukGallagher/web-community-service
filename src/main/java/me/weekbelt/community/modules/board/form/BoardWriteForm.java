6a1e40ebb3166e6c62e94abfbe216d32e713fdf2
weekbelt <vfrvfr4207@gmail.com>
2020-07-13 21:17:54 +0900

    custom content validation error message

--- src/main/java/me/weekbelt/community/modules/board/form/BoardWriteForm.java
+++ src/main/java/me/weekbelt/community/modules/board/form/BoardWriteForm.java
@@ -16,7 +16,7 @@ public class BoardWriteForm {
     private String title;
 
     @NotBlank
-    @Length(min = 10)
+    @Length(min = 10, message = "10자 이상 입력해 주세요.")
     private String content;
 
     private BoardType boardType;
