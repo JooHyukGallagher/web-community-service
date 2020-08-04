478ff68c9f1d299acbdaa69d9d9c36a77bdd6312
weekbelt <vfrvfr4207@gmail.com>
2020-08-04 18:19:57 +0900

    create Ajax request method

---
+++ src/main/resources/static/js/global.js
@@ -0,0 +1,29 @@
+const ajax = async (method, url, data, property) => {
+    let response;
+    try {
+        if(data == null) {
+            response = await fetch(url, {
+                method: method
+            });
+        } else {
+            response = await fetch(url, {
+                method: method,
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            });
+        }
+        const responseData = await response.json();
+
+        if (property == null) {
+            return responseData;
+        }
+        return responseData[property];
+    } catch (e) {
+        console.log("Error:", e);
+    }
+}
+// document.addEventListener("DOMContentLoaded", () => {
+//    initGlobalEvent();
+// });
