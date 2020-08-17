6c0e704279c0d451f2dbe869dde7c0d0eb8e4302
weekbelt <vfrvfr4207@gmail.com>
2020-08-17 20:14:38 +0900

    refactoring ajax request

--- src/main/resources/static/js/global.js
+++ src/main/resources/static/js/global.js
@@ -1,28 +1,54 @@
+// const ajax = async (method, url, data, property) => {
+//     const token = document.querySelector("#_csrf").content;
+//
+//     let response;
+//     try {
+//         if (data == null) {
+//             response = await fetch(url, {
+//                 method: method,
+//                 headers: {
+//                     'Content-Type': 'application/json',
+//                     'X-CSRF-TOKEN': token
+//                 },
+//             });
+//         } else {
+//             response = await fetch(url, {
+//                 method: method,
+//                 headers: {
+//                     'Content-Type': 'application/json',
+//                     'X-CSRF-TOKEN': token
+//                 },
+//                 body: JSON.stringify(data)
+//             });
+//         }
+//         const responseData = await response.json();
+//
+//         if (property == null) {
+//             return responseData;
+//         }
+//         return responseData[property];
+//     } catch (e) {
+//         console.log("Error:", e);
+//     }
+// }
+
 const ajax = async (method, url, data, property) => {
     const token = document.querySelector("#_csrf").content;
+    const init = {
+        method: method,
+        headers: {
+            'Content-Type': 'application/json',
+            'X-CSRF-TOKEN': token
+        }
+    }
+    if (data !== null) {
+        init.body = JSON.stringify(data);
+    }
 
     let response;
     try {
-        if(data == null) {
-            response = await fetch(url, {
-                method: method,
-                headers: {
-                    'Content-Type': 'application/json',
-                    'X-CSRF-TOKEN': token
-                },
-            });
-        } else {
-            response = await fetch(url, {
-                method: method,
-                headers: {
-                    'Content-Type': 'application/json',
-                    'X-CSRF-TOKEN': token
-                },
-                body: JSON.stringify(data)
-            });
-        }
+        response = await fetch(url, init);
         const responseData = await response.json();
-
         if (property == null) {
             return responseData;
         }
@@ -31,7 +57,3 @@ const ajax = async (method, url, data, property) => {
         console.log("Error:", e);
     }
 }
-
-// document.addEventListener("DOMContentLoaded", () => {
-//    initGlobalEvent();
-// });
