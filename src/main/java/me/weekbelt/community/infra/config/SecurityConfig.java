fdc669b0f31d5ad335ccb5742e7b1193acb90601
weekbelt <vfrvfr4207@gmail.com>
2020-10-28 21:15:52 +0900

    refactor: frontend template

--- src/main/java/me/weekbelt/community/infra/config/SecurityConfig.java
+++ src/main/java/me/weekbelt/community/infra/config/SecurityConfig.java
@@ -56,6 +56,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public void configure(WebSecurity web) throws Exception {
         web.ignoring()
                 .mvcMatchers("/node_modules/**")
+                .antMatchers("/assets/**")
                 .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 }
