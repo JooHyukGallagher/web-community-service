aeec8c02a5a59802cc24aa5ccfc7b5d0d4ae8c62
weekbelt <vfrvfr4207@gmail.com>
2020-10-31 09:51:04 +0900

    refactor: readForm

--- src/main/resources/static/assets/js/global.js
+++ src/main/resources/static/assets/js/global.js
@@ -1,37 +1,3 @@
-// const ajax = async (method, url, data, property) => {
-//     const token = document.querySelector("#_csrf").content;
-//
-//     let response;
-//     try {
-//         if (data == null) {
-//             response = await fetch(url, {
-//                 method: method,
-//                 headers: {
-//                     'Content-Type': 'application/json',
-//                     'X-CSRF-TOKEN': token
-//                 },
-//             });
-//         } else {
-//             response = await fetch(url, {
-//                 method: method,
-//                 headers: {
-//                     'Content-Type': 'application/json',
-//                     'X-CSRF-TOKEN': token
-//                 },
-//                 body: JSON.stringify(data)
-//             });
-//         }
-//         const responseData = await response.json();
-//
-//         if (property == null) {
-//             return responseData;
-//         }
-//         return responseData[property];
-//     } catch (e) {
-//         console.log("Error:", e);
-//     }
-// }
-
 const ajax = async (method, url, data, property) => {
     const token = document.querySelector("#_csrf").content;
     const init = {
