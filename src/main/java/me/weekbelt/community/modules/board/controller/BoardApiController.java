648a9000fd0ac82a4f46a8f8dea28af1d3f1ea98
weekbelt <vfrvfr4207@gmail.com>
2020-12-15 22:24:35 +0900

    refactor: Add location header

--- src/main/java/me/weekbelt/community/modules/board/controller/BoardApiController.java
+++ src/main/java/me/weekbelt/community/modules/board/controller/BoardApiController.java
@@ -19,7 +19,10 @@ import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.net.URI;
 import java.util.Collection;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 
 @RestController
 @RequestMapping(value = "/api/v1/boards")
@@ -67,14 +70,16 @@ public class BoardApiController {
             if (authorities.contains(new SimpleGrantedAuthority("ROLE_ADMIN"))) {
                 Long boardId = boardService.createBoard(account, boardWriteForm);
                 BoardReadForm boardReadForm = boardService.findBoardReadFormById(boardId);
-                return ResponseEntity.ok().body(boardReadForm);
+                URI location = linkTo(BoardApiController.class).slash(boardId).toUri();
+                return ResponseEntity.created(location).body(boardReadForm);
             } else {
                 return ResponseEntity.badRequest().build();
             }
         } else {
             Long boardId = boardService.createBoard(account, boardWriteForm);
             BoardReadForm boardReadForm = boardService.findBoardReadFormById(boardId);
-            return ResponseEntity.ok().body(boardReadForm);
+            URI location = linkTo(BoardApiController.class).slash(boardId).toUri();
+            return ResponseEntity.created(location).body(boardReadForm);
         }
     }
 }
