6f818f0d321d61a9f92fddffb9e9d09278f93898
posicube-joohyuk <weekbelt@posicube.com>
2020-12-20 19:41:23 +0900

    refactor: Add ErrorCode

--- src/main/java/me/weekbelt/community/infra/error/ErrorCode.java
+++ src/main/java/me/weekbelt/community/infra/error/ErrorCode.java
@@ -11,7 +11,8 @@ public enum ErrorCode {
     // Common
     SERVICE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "500", "Unspecified error", "ERROR"),
     INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST, "400", "Invalid Parameter", "ERROR"),
-    NOT_IMPLEMENTED(HttpStatus.NOT_IMPLEMENTED, "501", "Not Implemented yet", "ERROR");
+    NOT_IMPLEMENTED(HttpStatus.NOT_IMPLEMENTED, "501", "Not Implemented yet", "ERROR"),
+    NOT_FOUND(HttpStatus.NOT_FOUND, "404", "Missing Resource", "ERROR");
 
     private final HttpStatus httpStatus;
     private final String code;
